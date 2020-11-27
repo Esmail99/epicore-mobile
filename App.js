1e02e4f099a2ed95407419cdbf2e20ef4c2a78fe
Esmail <elmoussel@gmail.com>
2020-11-27 03:21:21 +0200

    finish coupon screen design

--- App.js
+++ App.js
@@ -1,113 +1,121 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
   Text,
-  StatusBar,
+  Image,
+  View,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showCode: false,
+    };
+  }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+  render() {
+    const {showCode} = this.state;
+
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.restaurantInfo}>
+          <Image
+            source={require('./assets/imgs/burger-king-logo.png')}
+            style={styles.restaurantLogo}
+          />
+          <View>
+            <Text style={styles.boldText}>Burger King</Text>
+            <Text style={[styles.text, styles.category]}>Food</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+        </View>
+        <Image
+          source={require('./assets/imgs/cover.jpg')}
+          style={styles.coverImage}
+        />
+        <View style={styles.discountContainer}>
+          {showCode ? (
+            <Text style={styles.code}>6587</Text>
+          ) : (
+            <TouchableOpacity
+              style={styles.discountButton}
+              onPress={() => {
+                this.setState({
+                  showCode: true,
+                });
+              }}>
+              <Text style={styles.text}>Discount</Text>
+            </TouchableOpacity>
+          )}
+          <Text style={styles.text}>1648 Favorites</Text>
+        </View>
+        <Text style={[styles.boldText, styles.description]}>
+          2 Big King Tower sandwiches for 45 EGP instead of 87 EGP
+        </Text>
+      </ScrollView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  text: {
+    fontSize: 20,
+    color: '#364343',
   },
-  body: {
-    backgroundColor: Colors.white,
+  boldText: {
+    fontSize: 25,
+    fontWeight: 'bold',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  restaurantInfo: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    paddingLeft: 25,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  restaurantLogo: {
+    width: 100,
+    resizeMode: 'contain',
+    marginRight: 7,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  category: {
+    fontSize: 15,
   },
-  highlight: {
-    fontWeight: '700',
+  coverImage: {
+    width: '100%',
+    resizeMode: 'contain',
+    marginTop: 15,
+    marginBottom: 30,
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  discountContainer: {
+    width: '100%',
+    paddingHorizontal: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  discountButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    borderWidth: 2,
+    borderRadius: 30,
+    borderColor: '#CDE126',
+  },
+  code: {
+    fontSize: 38,
+    color: '#CDE126',
+    fontWeight: 'bold',
+    marginRight: 25,
+  },
+  description: {
+    margin: 30,
   },
 });
 
